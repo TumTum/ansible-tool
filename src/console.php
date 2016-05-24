e7f41b01c5a82d847e21d034365125023ccae928
Tobias Matthaiou <matthaiou@solutiondrive.de>
2016-05-25 01:06:08 +0200

    Console System Installieren

---
+++ src/console.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created for ansible-tool
+ * Author: Tobias Matthaiou <matthaiou@tobimat.eu>
+ * Date: 25.05.16
+ * Time: 00:10
+ * Copyright: 2014 Tobias Matthaiou
+ */
+
+require __DIR__ . "/../vendor/autoload.php";
+
+$app = new Symfony\Component\Console\Application('ansible-tool', 'v0.0.1');
+$app->run();
